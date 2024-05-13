cb8ba13ac4c8924e93896f7c03fdd5e5b930b6f7
itsowavy <qndldtmsl3138@gmail.com>
2024-05-14 08:54:54 +0900

    feat: index, pth download 로직 추가

--- app/service.py
+++ app/service.py
@@ -1,2 +1,9 @@
-# def get_speakers_list():
-#
+import os
+
+from app.constants import PTH_DIR_PATH, INDEX_DIR_PATH
+from app.utils import download_from_s3
+
+
+def download_index_pth(speaker_name: str):
+    download_from_s3(f"pth/{speaker_name}.pth", os.path.join(PTH_DIR_PATH, f"{speaker_name}.pth"))
+    download_from_s3(f"index/{speaker_name}.index", os.path.join(INDEX_DIR_PATH, f"{speaker_name}.index"))
