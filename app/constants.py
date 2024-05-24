7edb84d1069ad0ff2212a8a8cf2f504701f107cb
itsowavy <qndldtmsl3138@gmail.com>
2024-05-24 15:18:49 +0900

    chore: data file 위치 AppData로 이동

--- app/constants.py
+++ app/constants.py
@@ -1,9 +1,10 @@
 import os
 
-PTH_DIR_PATH = 'models/pth'
-INDEX_DIR_PATH = 'models/index'
-DATA_DIR_PATH = 'data'
-SETTING_FILE_PATH = 'data/settings.json'
-SPEAKERS_FILE_PATH = 'data/speakers.json'
+APP_NAME = 'VoiceChanger'
+DATA_DIR_PATH = os.path.join(os.getenv('LOCALAPPDATA'), APP_NAME)
+PTH_DIR_PATH = os.path.join(DATA_DIR_PATH, 'pth')
+INDEX_DIR_PATH = os.path.join(DATA_DIR_PATH, 'index')
+SETTING_FILE_PATH = os.path.join(DATA_DIR_PATH, 'settings.json')
+SPEAKERS_FILE_PATH = os.path.join(DATA_DIR_PATH, 'speakers.json')
 AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
 AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
